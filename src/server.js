7914b3b7a7bec5347440d21143edb1e0dc651d04
Jeffery Cosby <jcoz1968@gmail.com>
2019-06-16 17:24:05 -0500

    route params

--- src/server.js
+++ src/server.js
@@ -9,8 +9,12 @@ app.get('/hello', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/hello/:name', (req, res) => { 
+  res.send(`Hello ${req.params.name}`);
+});
+
 app.post('/hello', (req, res) => { 
-  res.send(`hello ${req.body.name}!`);
+  res.send(`Hello ${req.body.name}!`);
 });
 
 app.listen(8000, () => { 
